e16ef4cb1eb5784c774110986609e06919f2fbbb
Ilija Loncarevic <ilija@loncarevic.me>
2018-11-30 16:27:00 +0100

    Updates

---
+++ precache-manifest.6eb8ec24e930baf96115a29d74c874da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e463cb76010f3421ce64",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "e463cb76010f3421ce64",
+    "url": "/contactmanager/static/js/main.e463cb76.chunk.js"
+  },
+  {
+    "revision": "3dfb54262f244d53fedb",
+    "url": "/contactmanager/static/css/1.8bc676df.chunk.css"
+  },
+  {
+    "revision": "3dfb54262f244d53fedb",
+    "url": "/contactmanager/static/js/1.3dfb5426.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "59f978205efaca55215a317526f51e60",
+    "url": "/contactmanager/index.html"
+  }
+];
